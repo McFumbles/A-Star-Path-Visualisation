4587253957e3dc2a8160efe7fc4db82ffc167a5a
Maverick <tayqinyuan@gmail.com>
2022-03-23 16:31:36 +0000

    Update update_neighbor function

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class Node:
   
   def update_neighbors(self, grid):
     self.neighbors = []
-    if self.row < self.total_rows - 1 and not grid[self.row - 1][self.col].is_barrier(): # DOWN
+    if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # DOWN
       self.neighbors.append(grid[self.row + 1][self.col])
 
     if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
@@ -81,7 +81,7 @@ class Node:
     if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # RIGHT
       self.neighbors.append(grid[self.row][self.col + 1])
 
-    if self.row > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
+    if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
       self.neighbors.append(grid[self.row][self.col - 1])
 
   def __lt__(self, other):
