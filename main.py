dc6f357b0e8adfd644acac176d4cff2f8de58353
Maverick <tayqinyuan@gmail.com>
2022-03-23 16:30:39 +0000

    Add algorithm function

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from platform import node
 import pygame
 import math
 from queue import PriorityQueue
@@ -93,6 +92,51 @@ def h(p1, p2):
   x2, y2 = p2
   return abs(x1 - x2) + abs(y1 - y2)
 
+def algorithm(draw, grid, start, end):
+  count = 0
+  open_set = PriorityQueue()
+  open_set.put((0, count, start))
+  came_from = {}
+
+  g_score = {node: float("inf") for row in grid for node in row}
+  g_score[start] = 0
+  
+  f_score = {node: float("inf") for row in grid for node in row}
+  f_score[start] = h(start.get_pos(), end.get_pos())
+
+  open_set_hash = {start}
+
+  while not open_set.empty():
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+      
+    current = open_set.get()[2]
+    open_set_hash.remove(current)
+
+    if current == end:
+      return True
+    
+    for neighbor in current.neighbors:
+      temp_g_score = g_score[current] + 1
+
+      if temp_g_score < g_score[neighbor]:
+        came_from[neighbor] = current
+        g_score[neighbor] = temp_g_score
+        f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+        if neighbor not in open_set_hash:
+          count += 1
+          open_set.put((f_score[neighbor], count, neighbor))
+          open_set_hash.add(neighbor)
+          neighbor.make_open()
+
+    draw()
+
+    if current != start:
+      current.make_closed()
+
+  return False
+
 def make_grid(rows, width):
   grid = []
   gap = width // rows
@@ -177,10 +221,10 @@ def main(win, width):
           if event.key == pygame.K_SPACE and not started:
             for row in grid:
               for node in row:
-                node.update_neighbors()
+                node.update_neighbors(grid)
 
             algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-            
+
   pygame.quit()
 
 main(WIN, width)
